3f2144c8455691dc0d34dec273884c955e83de22
ArtyomSirius <artuom.shelepnoy@vk.com>
2025-03-05 19:17:54 +0700

    Выполнение через бинарник

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,48 @@
 #include "opcode_ids.hpp"
 #include <vector>
 
-int main() {
+
+int main(int argc, char* argv[]) {
+    // Проверяем, передан ли аргумент с путём к файлу
+    if (argc < 2) {
+        std::cerr << "Использование: " << argv[0] << " <путь_к_файлу>" << std::endl;
+        return 1;
+    }
+
+    // Открываем бинарный файл для чтения
+    std::ifstream file(argv[1], std::ios::binary);
+    if (!file) {
+        std::cerr << "Ошибка открытия файла: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    std::vector<std::variant<int, std::string>> data;
+
+    while (file) {
+        char marker;
+        file.read(&marker, sizeof(marker));  // Читаем маркер
+
+        if (file.eof()) break;  // Если файл закончился
+
+        if (marker == '\x01') {  // Если это число
+            int number;
+            file.read(reinterpret_cast<char*>(&number), sizeof(number));
+            data.push_back(number);
+        } else if (marker == '\x02') {  // Если это строка
+            std::string str;
+            char ch;
+            while (file.get(ch) && ch != '\0') {
+                str += ch;
+            }
+            data.push_back(str);
+        } else {
+            std::cerr << "Неизвестный маркер в файле!" << std::endl;
+            return 1;
+        }
+    }
+
     VM vm;
-
-    // Байт-код для теста: (2 + 3) * (5 - 1) / 2
-    std::vector<std::variant<int, std::string>> program = {
-        RAND,
-        PUSH, 1,
-        PUSH, 0,
-        SUB,
-        INC,
-        MOD,
-        PUSH, 0,
-        ADD,
-
-        PRINT,
-        HALT
-
-    };
-
-    vm.execute(program);
+    vm.execute(data);
 
     return 0;
 }
